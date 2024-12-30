c0957bb3aba8043e90daa059a546ac5bf59c14d5
nikita <niklafinskiy@gmail.com>
2024-12-30 22:50:32 +0100

    terraform eks cluster + networking resources

---
+++ .infrastructure/terraform/subnets.tf
@@ -0,0 +1,47 @@
+resource "aws_subnet" "private_zone_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/19"
+  availability_zone = local.zone1
+
+  tags = {
+    Name = "${local.environment}-private-${local.zone1}"
+    "kubernetes.io/cluster/${local.environment}-${local.eks_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "private_zone_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.32.0/19"
+  availability_zone = local.zone2
+
+  tags = {
+    Name                                                           = "${local.environment}-private-${local.zone2}"
+    "kubernetes.io/cluster/${local.environment}-${local.eks_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public_zone_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.64.0/19"
+  availability_zone = local.zone1
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                                                           = "${local.environment}-public-${local.zone1}"
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${local.environment}-${local.eks_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public_zone_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.96.0/19"
+  availability_zone = local.zone2
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                                                           = "${local.environment}-public-${local.zone2}"
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${local.environment}-${local.eks_name}" = "owned"
+  }
+}
